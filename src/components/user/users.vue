16770b55d00b0ae407e61b8183e4dc176aa1def0
u4250 <1055991127@qq.com>
2021-03-18 09:26:53 +0800

    bread

--- src/components/user/users.vue
+++ src/components/user/users.vue
@@ -1,5 +1,14 @@
 <template>
-<div>11</div>
+<el-row :gutter="20">
+  <el-col :span="16">
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+  <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+  <el-breadcrumb-item>用户管理</el-breadcrumb-item>
+  <el-breadcrumb-item>用户列表</el-breadcrumb-item>
+</el-breadcrumb>
+  </el-col>
+  <el-col :span="8"><div class="grid-content bg-purple"></div></el-col>
+</el-row>
 </template>
 <script>
 export default {
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,5 +1,12 @@
+
 import Vue from 'vue'
-import { Button, Form, FormItem, Input, Message, Container, Header, Aside, Main, Menu, Submenu, MenuItemGroup, MenuItem, Table, TableColumn } from 'element-ui'
+import {
+  Button,
+  Form, FormItem, Input, Message, Container,
+  Header, Aside, Main, Menu, Submenu,
+  MenuItemGroup, MenuItem, Table, TableColumn,
+  Breadcrumb, BreadcrumbItem
+} from 'element-ui'
 
 Vue.use(Button)
 Vue.use(Form)
@@ -15,4 +22,6 @@ Vue.use(MenuItemGroup)
 Vue.use(MenuItem)
 Vue.use(Table)
 Vue.use(TableColumn)
+Vue.use(Breadcrumb)
+Vue.use(BreadcrumbItem)
 Vue.prototype.$message = Message
